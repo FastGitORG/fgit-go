7a0a8a53a38507984d8a36358b9cda167588bd85
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2020-07-06 00:43:24 +0800

    [scripr & ci] add version & timestamp

--- build.bat
+++ build.bat
@@ -7,40 +7,45 @@ echo ===========================
 echo -^> Removing old files
 del /s /Q release > nul
 
-SET CGO_ENABLED=0
+set CGO_ENABLED=0
+
+:: See more in .\fgit.go main
+set /P version=<version
+for /F %%i in ('powershell -c Get-Date -Format "yyyyMMddHHmmss"') do ( set timestamp=%%i)
+
 echo -^> Compiling AMD64
-SET GOARCH=amd64
+set GOARCH=amd64
 
-SET GOOS=windows
+set GOOS=windows
 echo --^> Compiling Windows
-go build -ldflags="-s -w" -o release\fgit-windows-amd64.exe src\fgit.go 
-SET GOOS=darwin
+go build -ldflags="-s -w -X main.version=%version% -X main.timestamp=%timestamp%" -o release\fgit-windows-amd64.exe src\fgit.go 
+set GOOS=darwin
 echo --^> Compiling Darwin
-go build -ldflags="-s -w" -o release\fgit-darwin-amd64 src\fgit.go 
-SET GOOS=linux
+go build -ldflags="-s -w -X main.version=%version%" -o release\fgit-darwin-amd64 src\fgit.go 
+set GOOS=linux
 echo --^> Compiling Linux
-go build -ldflags="-s -w" -o release\fgit-linux-amd64 src\fgit.go 
+go build -ldflags="-s -w -X main.version=%version%" -o release\fgit-linux-amd64 src\fgit.go 
 
-SET GOARCH=386
+set GOARCH=386
 echo -^> Compiling 386
-SET GOOS=windows
+set GOOS=windows
 echo --^> Compiling Windows
-go build -ldflags="-s -w" -o release\fgit-windows-386.exe src\fgit.go 
-SET GOOS=linux
+go build -ldflags="-s -w -X main.version=%version%" -o release\fgit-windows-386.exe src\fgit.go 
+set GOOS=linux
 echo --^> Compiling Linux
-go build -ldflags="-s -w" -o release\fgit-linux-386 src\fgit.go 
+go build -ldflags="-s -w -X main.version=%version%" -o release\fgit-linux-386 src\fgit.go 
 
 
-SET GOARCH=arm
+set GOARCH=arm
 echo -^> Compiling ARM
-SET GOOS=linux
+set GOOS=linux
 echo --^> Compiling Linux
-go build -ldflags="-s -w" -o release\fgit-linux-arm src\fgit.go 
+go build -ldflags="-s -w -X main.version=%version%" -o release\fgit-linux-arm src\fgit.go 
 
-SET GOARCH=arm64
+set GOARCH=arm64
 echo -^> Compiling ARM64
-SET GOOS=linux
+set GOOS=linux
 echo --^> Compiling Linux
-go build -ldflags="-s -w" -o release\fgit-linux-arm64 src\fgit.go 
+go build -ldflags="-s -w -X main.version=%version%" -o release\fgit-linux-arm64 src\fgit.go 
 
 pause
